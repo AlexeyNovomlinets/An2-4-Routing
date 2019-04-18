2e09389c809654aa8a48198e01c1333324d97722
Dzmitry Samuilik <dzmitry.samuilik@thomsonreuters.com>
2019-04-18 17:18:47 +0300

    Task 23: Login Component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AboutComponent, MessagesComponent, PathNotFoundComponent } from './components';
+import {
+  AboutComponent,
+  LoginComponent,
+  MessagesComponent,
+  PathNotFoundComponent
+} from './components';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [AboutComponent, PathNotFoundComponent, MessagesComponent]
+  declarations: [
+    AboutComponent,
+    PathNotFoundComponent,
+    MessagesComponent,
+    LoginComponent
+  ]
 })
 export class LayoutModule { }
